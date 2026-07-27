830d814548d636815e91171e75689703defe5ff6
ProAlexfreak <girieven01@gmail.com>
2026-07-28 00:08:19 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+app = Client(
+    "MultiShortenerBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+if __name__ == "__main__":
+    print("🚀 Multi Shortener Bot Started!")
+    app.run()
