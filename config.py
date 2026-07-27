b94f676ac0aa85f1be93fc3332776919f7111c35
ProAlexfreak <girieven01@gmail.com>
2026-07-28 00:17:57 +0530

    Add DB_CHANNEL_ID and DB_MESSAGE_ID to config

--- config.py
+++ config.py
@@ -8,4 +8,6 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 OWNER_ID = int(os.getenv("OWNER_ID"))
+
 DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID"))
+DB_MESSAGE_ID = int(os.getenv("DB_MESSAGE_ID"))
